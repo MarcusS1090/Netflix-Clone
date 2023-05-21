5dc1f7e9544b4957de4943367e8d363e9fe5ef2c
Marcus Senju <thegladus323@gmail.com>
2023-05-21 14:23:18 -0500

    finish

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,90 +1,67 @@
-import { compare } from "bcrypt";
-import NextAuth, {AuthOptions} from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-import Credentials from 'next-auth/providers/credentials';
-
-//INICIAR SESION CON REDES SOCIALES
+import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import InstagramProvider from 'next-auth/providers/instagram';
-import FacebookProvider from 'next-auth/providers/facebook';
+import Credentials from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { compare } from 'bcrypt';
+import prismadb from '@/lib/prismadb';
 
-
-
-import prismadb from "@/lib/prismadb";
-
-export const authOptions: AuthOptions = ({
+export const authOptions: AuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || ''
+            clientSecret: process.env.GITHUB_SECRET || '',
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
-        /*
-        InstagramProvider({
-            clientId: process.env.INSTAGRAM_CLIENT_ID || '',
-            clientSecret: process.env.INSTAGRAM_CLIENT_SECRET || '',
-        }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID || '',
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
-        }),
-        */
         Credentials({
             id: 'credentials',
             name: 'Credentials',
             credentials: {
-                email: {
-                    label: 'Email',
-                    type: 'text',
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password',
-                }
+            email: {
+                label: 'Email',
+                type: 'text',
             },
-            //vamos a checar si tenemos nuestras credenciales
+            password: {
+                label: 'Password',
+                type: 'passord'
+            }
+            },
             async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error('Email and password required');
-                }
+            if (!credentials?.email || !credentials?.password) {
+                throw new Error('Email and password required');
+            }
 
-                const user = await prismadb.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
+            const user = await prismadb.user.findUnique({ where: {
+                email: credentials.email
+            }});
 
-                if (!user || !user.hashedPassword) {
-                    throw new Error('Email does not exist');
-                }
+            if (!user || !user.hashedPassword) {
+                throw new Error('Email does not exist');
+            }
 
-                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
-                
-                if (!isCorrectPassword) {
-                    throw new Error('Incorrect password');
-                }
+            const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+            if (!isCorrectPassword) {
+                throw new Error('Incorrect password');
+            }
 
                 return user;
             }
         })
-    ],
-    pages: {
-        signIn: '/auth',
-    },
-    debug: process.env.NODE_ENV === 'development',
-    adapter: PrismaAdapter(prismadb),
-    session: {
-        strategy: 'jwt',
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET,
-    },
-    secret: process.env.NEXTAUTH_SECRET
-});
+  ],
+  pages: {
+    signIn: '/auth'
+  },
+  debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
+  session: { strategy: 'jwt' },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
 
 export default NextAuth(authOptions);
